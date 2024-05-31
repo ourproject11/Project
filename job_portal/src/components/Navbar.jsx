d6ad890d254090a7ce97f1af08ae4d9005d79489
sa11748 <shubhangishubhi490@gmail.com>
2024-05-31 11:43:54 +0530

    Completed work on responsive navbar design and add some stylings on app.css

--- job_portal/src/components/Navbar.jsx
+++ job_portal/src/components/Navbar.jsx
@@ -1,23 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FaBars, FaXmark } from "react-icons/fa6";
+import '../App.css';
 
 const Navbar = () => {
-    const [isMenuOpen , setIsMenuOpen] = useState(false);
-    const handleMenuToggler = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  const navItems = [
+    { path: "/", title: "Job Search" },
+    { path: "/my", title: "My Jobs" },
+    { path: "/salary", title: "Salary" },
+    { path: "/post", title: "Hire" },
+  ];
+
   return (
-    <header>
-     <nav>
-        <a href= "/" className= "flex items-center gap-2 text-2xl" >
-        <img 
-        src="/images/file.png" 
-        width="29" 
-        height="30" 
-        alt="Icon description"
-        />
-    <span>Talent Track</span>
-    </a>
-    </nav> 
+    <header className="container xl:px-24 px-4">
+      <nav className="header">
+        <a href="/" className="flex items-center gap-2 text-2xl text-black">
+          <img 
+            src="/images/file.png" 
+            width="29" 
+            height="30" 
+            alt="Icon description"
+          />
+          <span>HireHub</span>
+        </a>
+        {/* Nav items for large devices */}
+        <ul className="nav-items">
+          {navItems.map(({ path, title }) => (
+            <li key={path} className="nav-item">
+              <NavLink 
+                to={path} 
+                className={({ isActive }) => isActive ? "active" : ""}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+
+        {/* Signup and login */}
+        <div className="auth-links">
+          <Link to="/login" className="py-2 px-5 border rounded">Log In</Link>
+          <Link to="/signup" className="py-2 px-5 border rounded signup">Signup</Link>
+        </div>
+        {/* mobile menu */}
+        <div className="mobile-menu-button">
+          <button onClick={handleMenuToggler}>
+            {isMenuOpen ? <FaXmark className="w-5 h-5 text-primary" /> : <FaBars className="w-5 h-5 text-primary" />}
+          </button>
+        </div>
+      </nav>
+
+      {/* Nav items for mobile version */}
+      <div className={`mobile-menu ${isMenuOpen ? "" : "hidden"}`}>
+        <ul>
+          {navItems.map(({ path, title }) => (
+            <li key={path} className="text-base text-white py-1">
+              <NavLink 
+                to={path} 
+                className={({ isActive }) => isActive ? "active" : ""}
+              >
+                {title}
+              </NavLink>
+            </li>
+          ))}
+          <li className="text-white py-1">
+            <Link to="/login">Log In</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
